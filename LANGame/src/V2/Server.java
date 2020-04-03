222c634905fb1a26a0cd99afad51423d9c418dac
moo32 <38340314+moo32@users.noreply.github.com>
2020-04-03 11:57:01 -0400

    Update Server.java

--- LANGame/src/V2/Server.java
+++ LANGame/src/V2/Server.java
@@ -1,5 +1,9 @@
 package V2;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.function.Consumer;
 
 public class Server extends NetworkConnection
@@ -7,6 +11,8 @@ public class Server extends NetworkConnection
 
 	private int port;
 	String name;
+	
+//	ListenerThread listenerThread;
 
 	public Server(int port, String name, Consumer<Serializable> onReceiveCallback) {
 		super(onReceiveCallback);
@@ -28,6 +34,39 @@ public class Server extends NetworkConnection
 	protected int getPort() {
 		return this.port;
 	}
+	
+//	public class ListenerThread extends Thread
+//	{
+//		private Socket socket;
+//		private ObjectOutputStream out;
+//		
+//		
+//		@Override
+//		public void run()
+//		{
+//			try(ServerSocket server = isServer() ? new ServerSocket(getPort()) : null;
+//					Socket socket = isServer() ? server.accept() : new Socket(getIP(), getPort());
+//					ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+//					ObjectInputStream in = new ObjectInputStream(socket.getInputStream()) )
+//			{
+//				this.socket = socket;
+//				this.out = out;
+//				socket.setTcpNoDelay(true);
+//				
+//				
+//			// infinite while loop reading in objects
+//				while(true) {
+//					Serializable data = (Serializable) in.readObject();
+//					onRecieveCallback.accept(data);
+//				}
+//				
+//			}
+//			catch(Exception e)
+//			{
+//				onRecieveCallback.accept("Connection closed");
+//			}
+//		}
+//	}
 	
 	
 }
